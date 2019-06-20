6df908f4040434a65a98e828f74381a3472e2554
Alex Bogdan <stefanalexb@gmail.com>
2019-06-20 16:36:02 -0400

    First react component in and made list of node requirements

--- application/static/js/components/Home.jsx
+++ application/static/js/components/Home.jsx
@@ -1,8 +1,49 @@
-import React, { Component } from 'react';
-export default class Home extends Component {
-    render() {
-       return (
-          <h1>Hello React!</h1>
-       )
-    }
+// import React, { Component } from 'react';
+// export default class Home extends Component {
+//     render() {
+//        return (
+//           <h1>Hello React the2 seqel!</h1>
+//        )
+//     }
+// }
+
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { Link } from 'react-router'
+
+export default function Home() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+  function handleRedirect(url){
+    location.href=url;
+  }
+
+  return (
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Open Menu
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={() => {handleRedirect('auth/login')}}>Login</MenuItem>
+        <MenuItem onClick={() => {handleRedirect('/')}}>Home</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
 }
